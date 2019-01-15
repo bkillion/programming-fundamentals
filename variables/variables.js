b673a22518ebcf1d9a8585428ff0bbd70deb91c6
Brian Killion <bkillion@e-venture-inc.com>
2019-01-14 18:19:18 -0700

    initial commit - variables

---
+++ variables/variables.js
@@ -0,0 +1,9 @@
+var userName = "Albert";
+const totalXP = 2000;
+let myXP = 1880;
+
+console.log("Player Name: ", userName);
+console.log("XP: " + myXP + "/" + totalXP);
+
+//variable naming rules
+//Only allowed as first characters: $ _ a (through) z A (through) Z
